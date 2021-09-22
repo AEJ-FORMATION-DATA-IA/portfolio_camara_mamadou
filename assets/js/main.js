6ff1f2148d51a199c95bade57c7d6096394c914a
mamadou camara <50184689+rukov42@users.noreply.github.com>
2021-09-22 15:58:47 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/*===== MENU SHOW =====*/ 
+/*===== MENU SHOW =====*/
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -12,13 +12,13 @@ const showMenu = (toggleId, navId) =>{
 showMenu('nav-toggle','nav-menu')
 
 /*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');   
+const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction(){
   /*Active link*/
   navLink.forEach(n => n.classList.remove('active'));
   this.classList.add('active');
-  
+
   /*Remove menu mobile*/
   const navMenu = document.getElementById('nav-menu')
   navMenu.classList.remove('show')
@@ -33,25 +33,25 @@ const sr = ScrollReveal({
     reset: true
 });
 
-/*SCROLL HOME*/
-sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+/*SCROLL ACCUEIL*/
+sr.reveal('.accueil__title',{});
+sr.reveal('.button',{delay: 200});
+sr.reveal('.accueil__img',{delay: 400});
+sr.reveal('.accueil__social-icon',{ interval: 200});
 
-/*SCROLL ABOUT*/
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+/*SCROLL A PROPOS DE MOI*/
+sr.reveal('.a_propos_de_moi__img',{});
+sr.reveal('.a_propos_de_moi__subtitle',{delay: 400});
+sr.reveal('.a_propos_de_moi__text',{delay: 400});
 
-/*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+/*SCROLL COMPETENCE*/
+sr.reveal('.competence__subtitle',{});
+sr.reveal('.competence__text',{});
+sr.reveal('.competence__data',{interval: 200});
+sr.reveal('.competence__img',{delay: 600});
 
-/*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
+/*SCROLL TRAVAUX*/
+sr.reveal('.travaux__img',{interval: 200});
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 200}); 
+sr.reveal('.contact__input',{interval: 200});
